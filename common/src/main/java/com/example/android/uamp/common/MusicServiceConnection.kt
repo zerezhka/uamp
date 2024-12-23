7a4588c69f247cc3aaabfc13da541848915cb8db
Sergey Belyaev <svbelyaev@avito.ru>
2024-12-24 03:06:20 +0400

    builds

--- common/src/main/java/com/example/android/uamp/common/MusicServiceConnection.kt
+++ common/src/main/java/com/example/android/uamp/common/MusicServiceConnection.kt
@@ -111,14 +111,14 @@ class MusicServiceConnection(context: Context, serviceComponent: ComponentName) 
             ?: ImmutableList.of()
     }
 
-    suspend fun sendCommand(command: String, parameters: Bundle?):Boolean =
+    suspend fun sendCommand(command: String, parameters: Bundle?): Boolean =
         sendCommand(command, parameters) { _, _ -> }
 
     suspend fun sendCommand(
         command: String,
         parameters: Bundle?,
         resultCallback: ((Int, Bundle?) -> Unit)
-    ):Boolean = if (browser?.isConnected == true) {
+    ): Boolean = if (browser?.isConnected == true) {
         val args = parameters ?: Bundle()
         browser?.sendCustomCommand(SessionCommand(command, args), args)?.await()?.let {
             resultCallback(it.resultCode, it.extras)
@@ -188,7 +188,8 @@ class MusicServiceConnection(context: Context, serviceComponent: ComponentName) 
         override fun onEvents(player: Player, events: Player.Events) {
             if (events.contains(EVENT_PLAY_WHEN_READY_CHANGED)
                 || events.contains(EVENT_PLAYBACK_STATE_CHANGED)
-                || events.contains(EVENT_MEDIA_ITEM_TRANSITION)) {
+                || events.contains(EVENT_MEDIA_ITEM_TRANSITION)
+            ) {
                 updatePlaybackState(player)
                 if (player.playbackState != Player.STATE_IDLE) {
                     networkFailure.postValue(false)
@@ -196,13 +197,14 @@ class MusicServiceConnection(context: Context, serviceComponent: ComponentName) 
             }
             if (events.contains(EVENT_MEDIA_METADATA_CHANGED)
                 || events.contains(EVENT_MEDIA_ITEM_TRANSITION)
-                || events.contains(EVENT_PLAY_WHEN_READY_CHANGED)) {
+                || events.contains(EVENT_PLAY_WHEN_READY_CHANGED)
+            ) {
                 updateNowPlaying(player)
             }
         }
 
         override fun onPlayerErrorChanged(error: PlaybackException?) {
-            when(error?.errorCode) {
+            when (error?.errorCode) {
                 ERROR_CODE_IO_BAD_HTTP_STATUS,
                 ERROR_CODE_IO_INVALID_HTTP_CONTENT_TYPE,
                 ERROR_CODE_IO_CLEARTEXT_NOT_PERMITTED,
@@ -218,7 +220,8 @@ class MusicServiceConnection(context: Context, serviceComponent: ComponentName) 
 class PlaybackState(
     private val playbackState: Int = Player.STATE_IDLE,
     private val playWhenReady: Boolean = false,
-    val duration: Long = C.TIME_UNSET) {
+    val duration: Long = C.TIME_UNSET
+) {
     val isPlaying: Boolean
         get() {
             return (playbackState == Player.STATE_BUFFERING
@@ -227,8 +230,6 @@ class PlaybackState(
         }
 }
 
-@Suppress("PropertyName")
 val EMPTY_PLAYBACK_STATE: PlaybackState = PlaybackState()
 
-@Suppress("PropertyName")
 val NOTHING_PLAYING: MediaItem = MediaItem.EMPTY
